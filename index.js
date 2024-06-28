4595e1463f94bada72d6f60e0463e2394b2eaaeb
Robin Danzinger <robin.danzinger@gmx.com>
2024-06-28 16:29:05 +0200

    send decorator and afterRenderHook in updateComponent

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-async function create(afterRenderHook, decorators) {
+async function create() {
   try {
     const { createApp } = await import('vue')
     const { default: VueApp } = await import('./src/VueRenderer.vue')
 
-    let app = createApp(VueApp, { afterRenderHook, decorators })
+    let app = createApp(VueApp)
     app.mount('#garden_app')
     return {
       destroy: () => app?.unmount?.(),
       updateComponent: (props) => {
         app?.unmount()
-        app = createApp(VueApp, { ...props, afterRenderHook, decorators })
+        app = createApp(VueApp, { ...props })
         app.mount('#garden_app')
       },
     }
