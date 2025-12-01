cd0edffbc9f9881cc9933c140d339fa2a2e375b9
Robin Danzinger <robin.danzinger@gmx.com>
2025-12-01 12:03:42 +0100

    Add optional setup function to renderer,
    so vue plugins can be added to the created app

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-async function create() {
+async function create(setup) {
   try {
     const { createApp } = await import('vue')
     const { default: VueApp } = await import('./src/VueRenderer.vue')
 
     let app = createApp(VueApp)
+    setup?.(app)
     app.mount('#garden_app')
     return {
       destroy: () => app?.unmount?.(),
       updateComponent: (props) => {
         app?.unmount()
         app = createApp(VueApp, { ...props })
+        setup?.(app)
         app.mount('#garden_app')
       },
     }
